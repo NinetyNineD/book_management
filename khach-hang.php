7c08653c3dcc981933644d72440328195c394158
Nguyen Trung Duc <trungduc111bx@gmail.com>
2020-03-01 14:43:09 +0700

    Hiển thị dữ liệu và phân trang

--- khach-hang.php
+++ khach-hang.php
@@ -40,7 +40,7 @@
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
       <!-- Sidebar - Brand -->
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
         <div class="sidebar-brand-icon rotate-n-15">
           <i class="fas fa-laugh-wink"></i>
         </div>
@@ -54,7 +54,7 @@
 
       <!-- Nav Item - Tables -->
       <li class="nav-item active">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="index.php">
           <i class="fas fa-fw fa-table"></i>
           <span>Admin</span></a>
       </li>
@@ -63,7 +63,7 @@
       <hr class="sidebar-divider d-none d-md-block">
       <!-- Nav Item - Tables -->
       <li class="nav-item active">
-        <a class="nav-link" href="tac-gia.html">
+        <a class="nav-link" href="tac-gia.php">
           <i class="fas fa-fw fa-table"></i>
           <span>Tác giả</span></a>
       </li>
@@ -72,7 +72,7 @@
       <hr class="sidebar-divider d-none d-md-block">
       <!-- Nav Item - Tables -->
       <li class="nav-item active">
-        <a class="nav-link" href="san-pham.html">
+        <a class="nav-link" href="san-pham.php">
           <i class="fas fa-fw fa-table"></i>
           <span>Sản phẩm</span></a>
       </li>
@@ -81,7 +81,7 @@
       <hr class="sidebar-divider d-none d-md-block">
       <!-- Nav Item - Tables -->
       <li class="nav-item active">
-        <a class="nav-link" href="khach-hang.html">
+        <a class="nav-link" href="khach-hang.php">
           <i class="fas fa-fw fa-table"></i>
           <span>Khách hàng</span></a>
       </li>
@@ -90,7 +90,7 @@
       <hr class="sidebar-divider d-none d-md-block">
       <!-- Nav Item - Tables -->
       <li class="nav-item active">
-        <a class="nav-link" href="nhan-vien.html">
+        <a class="nav-link" href="nhan-vien.php">
           <i class="fas fa-fw fa-table"></i>
           <span>Nhân viên</span></a>
       </li>
@@ -151,6 +151,27 @@
             Thêm mới
           </button>
 
+
+            <?php
+            $conn = mysqli_connect('localhost', 'root', '', 'bookmanagement');
+
+            if (isset($_POST['INSERT'])){
+
+                $name_customer_insert = $_POST['name_customer_insert'];
+                $phone_customer_insert = $_POST['phone_customer_insert'];
+                $email_customer_insert = $_POST['email_customer_insert'];
+                $address_customer_insert = $_POST['address_customer_insert'];
+
+
+                $insert = mysqli_query($conn,"INSERT INTO customer(NAME_CUSTOMER,PHONE_CUSTOMER,EMAIL_CUSTOMER,ADDRESS_CUSTOMER) VALUE (N'$name_customer_insert',N'$phone_customer_insert',N'$email_customer_insert',N'$address_customer_insert')");
+                if($insert){
+                    header('location:khach-hang.php');
+                }
+            }
+
+            ?>
+
+
           <!-- Modal -->
           <div class="modal fade" id="modelId" tabindex="-1" role="dialog" aria-labelledby="modelTitleId"
             aria-hidden="true">
@@ -163,82 +184,124 @@
                   </button>
                 </div>
                 <div class="modal-body">
-                  <form class=" row" >
+                  <form class=" row" method="post" >
                       <label class="col-sm-4 avc" for="name" style="font-weight: bold">Họ và Tên</label>
-                      <input class="col-sm-8 avc" type="text" name="name_customer" id="name" class="form-control" placeholder="" aria-describedby="helpId">
+                      <input class="col-sm-8 avc" type="text" name="name_customer_insert" id="name" class="form-control" placeholder="" aria-describedby="helpId">
 
 
 
                       <label class="col-sm-4 avc" for="phone" style="font-weight: bold">Số Điện Thoại</label>
-                      <input class="col-sm-8 avc" type="text" name="phone_customer" id="phone" class="form-control" placeholder="" aria-describedby="helpId">
+                      <input class="col-sm-8 avc" type="text" name="phone_customer_insert" id="phone" class="form-control" placeholder="" aria-describedby="helpId">
 
 
 
                       <label class="col-sm-4 avc" for="email" style="font-weight:bold ">Email</label>
-                      <input class="col-sm-8 avc" type="email" name="email_customer" id="email" class="form-control" placeholder="" aria-describedby="helpId">
+                      <input class="col-sm-8 avc" type="email" name="email_customer_insert" id="email" class="form-control" placeholder="" aria-describedby="helpId">
 
 
 
                       <label class="col-sm-4 avc" for="address" style="font-weight: bold">Địa Chỉ</label>
-                      <input class="col-sm-8 avc" type="text" name="addresss_customer" id="address" class="form-control" placeholder="" aria-describedby="helpId">
+                      <input class="col-sm-8 avc" type="text" name="addresss_customer_insert" id="address" class="form-control" placeholder="" aria-describedby="helpId">
 
 
                   </form>
                 </div>
                 <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary">Save</button>
+                    <input type="submit" name="INSERT" value="SAVE" style="background-color: #dfff2e">
                 </div>
               </div>
             </div>
           </div>
           <!-- DataTales Example -->
-          <div class="card shadow mb-4">
-            <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
-            </div>
-            <div class="card-body">
-              <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                  <thead>
-                    <tr>
-                      <th>Name</th>
-                      <th>Position</th>
-                      <th>Office</th>
-                      <th>Age</th>
-                      <th>Start date</th>
-                      <th>Salary</th>
-                      <th>Action</th>
-                    </tr>
-                  </thead>
 
-                  <tbody>
-                    <tr>
-                      <td>Tiger Nixon</td>
-                      <td>System Architect</td>
-                      <td>Edinburgh</td>
-                      <td>61</td>
-                      <td>2011/04/25</td>
-                      <td>$320,800</td>
-                      <td>
-                        <a name="" id="" class="btn btn-warning" href="#" role="button">Sửa</a>
-                        <a name="" id="" class="btn btn-danger" href="#" role="button">Xóa</a>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Garrett Winters</td>
-                      <td>Accountant</td>
-                      <td>Tokyo</td>
-                      <td>63</td>
-                      <td>2011/07/25</td>
-                      <td>$170,750</td>
-                    </tr>
+            <?php
+            // PHẦN XỬ LÝ PHP
+            // BƯỚC 1: KẾT NỐI CSDL
+            $conn = mysqli_connect('localhost', 'root', '', 'bookmanagement');
 
-                  </tbody>
+            // BƯỚC 2: TÌM TỔNG SỐ RECORDS
+            $result = mysqli_query($conn, 'select count(ID_CUSTOMER) as total from customer');
+            $row = mysqli_fetch_assoc($result);
+            $total_records = $row['total'];
+
+            // BƯỚC 3: TÌM LIMIT VÀ CURRENT_PAGE
+            $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
+            $limit = 10;
+
+
+            // BƯỚC 4: TÍNH TOÁN TOTAL_PAGE VÀ START
+            // tổng số trang
+            $total_page = ceil($total_records / $limit);
+
+            // Giới hạn current_page trong khoảng 1 đến total_page
+            if ($current_page > $total_page){
+                $current_page = $total_page;
+            }
+            else if ($current_page < 1){
+                $current_page = 1;
+            }
+
+            // Tìm Start
+            $start = ($current_page - 1) * $limit;
+
+            // BƯỚC 5: TRUY VẤN LẤY DANH SÁCH TIN TỨC
+            // Có limit và start rồi thì truy vấn CSDL lấy danh sách tin tức
+            $result = mysqli_query($conn, "SELECT * FROM customer LIMIT $start, $limit");
+
+            ?>
+            <div>
+                <table width="100%" border="1px" cellspacing="0px" cellpadding="20px">
+                    <tr>
+
+                        <th width="20%">Name</th>
+                        <th width="10%">Phone</th>
+                        <th width="30%">Email</th>
+                        <th width="30%">Address</th>
+                        <th colspan="2" width="10%">Option</th>
+                    </tr>
+                    <?php while ($row = mysqli_fetch_assoc($result)){ ?>
+                        <tr>
+
+                            <td><?php echo $row['NAME_CUSTOMER'] ?></td>
+                            <td><?php echo $row["PHONE_CUSTOMER"] ?></td>
+                            <td><?php echo $row["EMAIL_CUSTOMER"] ?></td>
+                            <td><?php echo $row["ADDRESS_CUSTOMER"] ?></td>
+                            <td><a href="update_customer.php?ID_CUSTOMER=<?php echo $row['ID_CUSTOMER'] ?>">Update</a></td>
+
+                            <td><a href="delete_customer.php?ID_CUSTOMER=<?php echo $row['ID_CUSTOMER']?>" onclick="if(confirm('Do you Sure ?')) return true; else return false">Delete</a></td>
+                        </tr>
+                    <?php } ?>
                 </table>
-              </div>
             </div>
-          </div>
+            <div  style="text-align: center; margin: auto">
+                <?php
+                // PHẦN HIỂN THỊ PHÂN TRANG
+                // BƯỚC 7: HIỂN THỊ PHÂN TRANG
+
+                // nếu current_page > 1 và total_page > 1 mới hiển thị nút prev
+                if ($current_page > 1 && $total_page > 1){
+                    echo '<a href="khach-hang.php?page='.($current_page-1).'">Prev</a> | ';
+                }
+
+                // Lặp khoảng giữa
+                for ($i = 1; $i <= $total_page; $i++){
+                    // Nếu là trang hiện tại thì hiển thị thẻ span
+                    // ngược lại hiển thị thẻ a
+                    if ($i == $current_page){
+                        echo '<span>'.$i.'</span> | ';
+                    }
+                    else{
+                        echo '<a href="khach-hang.php?page='.$i.'">'.$i.'</a> | ';
+                    }
+                }
+
+                // nếu current_page < $total_page và total_page > 1 mới hiển thị nút prev
+                if ($current_page < $total_page && $total_page > 1){
+                    echo '<a href="khach-hang.php?page='.($current_page+1).'">Next</a> | ';
+                }
+                ?>
+            </div>
+
 
         </div>
         <!-- /.container-fluid -->
