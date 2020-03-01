3e5dcd7c0668b0a3e8302b7292e4f3bb1d515454
Nguyen Trung Duc <trungduc111bx@gmail.com>
2020-03-01 17:27:12 +0700

    Hoàn thành phân trang và thêm mới khách hàng

--- khach-hang.php
+++ khach-hang.php
@@ -1,23 +1,20 @@
+<?php
+    ob_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-
   <title>Book Management</title>
-
   <!-- Custom fonts for this template -->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link
     href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
     rel="stylesheet">
-
-
   <!-- Custom styles for this template -->
   <link href="css/sb-admin-2.css" rel="stylesheet">
   <style>
@@ -27,18 +24,12 @@
       outline: none;
     }
   </style>
-
-
 </head>
-
 <body id="page-top">
-
   <!-- Page Wrapper -->
   <div id="wrapper">
-
     <!-- Sidebar -->
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
       <!-- Sidebar - Brand -->
       <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
         <div class="sidebar-brand-icon rotate-n-15">
@@ -46,20 +37,15 @@
         </div>
         <div class="sidebar-brand-text mx-3">Book Management</div>
       </a>
-
       <!-- Divider -->
       <hr class="sidebar-divider my-0">
-
-
-
       <!-- Nav Item - Tables -->
       <li class="nav-item active">
         <a class="nav-link" href="index.php">
           <i class="fas fa-fw fa-table"></i>
           <span>Admin</span></a>
       </li>
-
-      <!-- Divider -->
+        <!-- Divider -->
       <hr class="sidebar-divider d-none d-md-block">
       <!-- Nav Item - Tables -->
       <li class="nav-item active">
@@ -67,7 +53,6 @@
           <i class="fas fa-fw fa-table"></i>
           <span>Tác giả</span></a>
       </li>
-
       <!-- Divider -->
       <hr class="sidebar-divider d-none d-md-block">
       <!-- Nav Item - Tables -->
@@ -76,7 +61,6 @@
           <i class="fas fa-fw fa-table"></i>
           <span>Sản phẩm</span></a>
       </li>
-
       <!-- Divider -->
       <hr class="sidebar-divider d-none d-md-block">
       <!-- Nav Item - Tables -->
@@ -85,7 +69,6 @@
           <i class="fas fa-fw fa-table"></i>
           <span>Khách hàng</span></a>
       </li>
-
       <!-- Divider -->
       <hr class="sidebar-divider d-none d-md-block">
       <!-- Nav Item - Tables -->
@@ -94,37 +77,27 @@
           <i class="fas fa-fw fa-table"></i>
           <span>Nhân viên</span></a>
       </li>
-
       <!-- Divider -->
       <hr class="sidebar-divider d-none d-md-block">
-
       <!-- Sidebar Toggler (Sidebar) -->
       <div class="text-center d-none d-md-inline">
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
       </div>
-
     </ul>
     <!-- End of Sidebar -->
-
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
-
       <!-- Main Content -->
       <div id="content">
-
         <!-- Topbar -->
         <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
           <!-- Sidebar Toggle (Topbar) -->
           <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
             <i class="fa fa-bars"></i>
           </button>
-
           <!-- Topbar Search -->
-
           <!-- Topbar Navbar -->
           <ul class="navbar-nav ml-auto">
-
             <!-- Nav Item - Search Dropdown (Visible Only XS) -->
             <li class="nav-item dropdown no-arrow d-sm-none">
               <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown"
@@ -132,13 +105,10 @@
                 <i class="fas fa-search fa-fw"></i>
               </a>
               <!-- Dropdown - Messages -->
-
             </li>
           </ul>
-
         </nav>
         <!-- End of Topbar -->
-
         <!-- Begin Page Content -->
         <div class="container-fluid">
           <!-- Page Heading -->
@@ -150,28 +120,6 @@
           <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#modelId">
             Thêm mới
           </button>
-
-
-            <?php
-            $conn = mysqli_connect('localhost', 'root', '', 'bookmanagement');
-
-            if (isset($_POST['INSERT'])){
-
-                $name_customer_insert = $_POST['name_customer_insert'];
-                $phone_customer_insert = $_POST['phone_customer_insert'];
-                $email_customer_insert = $_POST['email_customer_insert'];
-                $address_customer_insert = $_POST['address_customer_insert'];
-
-
-                $insert = mysqli_query($conn,"INSERT INTO customer(NAME_CUSTOMER,PHONE_CUSTOMER,EMAIL_CUSTOMER,ADDRESS_CUSTOMER) VALUE (N'$name_customer_insert',N'$phone_customer_insert',N'$email_customer_insert',N'$address_customer_insert')");
-                if($insert){
-                    header('location:khach-hang.php');
-                }
-            }
-
-            ?>
-
-
           <!-- Modal -->
           <div class="modal fade" id="modelId" tabindex="-1" role="dialog" aria-labelledby="modelTitleId"
             aria-hidden="true">
@@ -183,32 +131,42 @@
                     <span aria-hidden="true">&times;</span>
                   </button>
                 </div>
+                  <?php
+                  $conn = mysqli_connect('localhost', 'root', '', 'bookmanagement');
+                  if (isset($_POST['INSERT'])){
+                      $name_customer_insert = $_POST['name_customer_insert'];
+                      $phone_customer_insert = $_POST['phone_customer_insert'];
+                      $email_customer_insert = $_POST['email_customer_insert'];
+                      $address_customer_insert = $_POST['address_customer_insert'];
+                      $insert = mysqli_query($conn,"INSERT INTO customer(ID_CUSTOMER,NAME_CUSTOMER,PHONE_CUSTOMER,EMAIL_CUSTOMER,ADDRESS_CUSTOMER) VALUE ('',N'$name_customer_insert',N'$phone_customer_insert',N'$email_customer_insert',N'$address_customer_insert')");
+                      if($insert){
+                          header('location:khach-hang.php');
+                          ob_end_flush();
+                      }
+                  }
+                  ?>
                 <div class="modal-body">
                   <form class=" row" method="post" >
+
                       <label class="col-sm-4 avc" for="name" style="font-weight: bold">Họ và Tên</label>
-                      <input class="col-sm-8 avc" type="text" name="name_customer_insert" id="name" class="form-control" placeholder="" aria-describedby="helpId">
-
-
+                      <input class="col-sm-8 avc" type="text" name="name_customer_insert" id="name" class="form-control" placeholder="Nhập họ và tên" aria-describedby="helpId">
 
                       <label class="col-sm-4 avc" for="phone" style="font-weight: bold">Số Điện Thoại</label>
-                      <input class="col-sm-8 avc" type="text" name="phone_customer_insert" id="phone" class="form-control" placeholder="" aria-describedby="helpId">
-
-
+                      <input class="col-sm-8 avc" type="text" name="phone_customer_insert" id="phone" class="form-control" placeholder="Nhập số điện thoại" aria-describedby="helpId">
 
                       <label class="col-sm-4 avc" for="email" style="font-weight:bold ">Email</label>
-                      <input class="col-sm-8 avc" type="email" name="email_customer_insert" id="email" class="form-control" placeholder="" aria-describedby="helpId">
-
-
+                      <input class="col-sm-8 avc" type="email" name="email_customer_insert" id="email" class="form-control" placeholder="Nhập email" aria-describedby="helpId">
 
                       <label class="col-sm-4 avc" for="address" style="font-weight: bold">Địa Chỉ</label>
-                      <input class="col-sm-8 avc" type="text" name="addresss_customer_insert" id="address" class="form-control" placeholder="" aria-describedby="helpId">
+                      <input class="col-sm-8 avc" type="text" name="address_customer_insert" id="address" class="form-control" placeholder="Nhập địa chỉ" aria-describedby="helpId">
 
-
+                      <div class="modal-footer">
+                          <input type="submit" name="INSERT" value="SAVE" style="background-color: #9afff5 ; margin-right: 10px">
+                      </div>
                   </form>
+
                 </div>
-                <div class="modal-footer">
-                    <input type="submit" name="INSERT" value="SAVE" style="background-color: #dfff2e">
-                </div>
+
               </div>
             </div>
           </div>
@@ -262,7 +220,7 @@
                     <?php while ($row = mysqli_fetch_assoc($result)){ ?>
                         <tr>
 
-                            <td><?php echo $row['NAME_CUSTOMER'] ?></td>
+                            <td><?php echo $row["NAME_CUSTOMER"] ?></td>
                             <td><?php echo $row["PHONE_CUSTOMER"] ?></td>
                             <td><?php echo $row["EMAIL_CUSTOMER"] ?></td>
                             <td><?php echo $row["ADDRESS_CUSTOMER"] ?></td>
