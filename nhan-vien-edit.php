5bd155183f734ff28d45f0de25743a070bf43810
NinetyNineD <nguyentandung.24h99p@gmail.com>
2020-03-01 21:51:14 +0700

    Code done, page edit with ajax

--- nhan-vien-edit.php
+++ nhan-vien-edit.php
@@ -1,13 +1,24 @@
 <?php
-	require_once "DAO/DBConnect.php";
-	$Code = $_POST['id'];
-	$result = array();
-  	$result = getAllList("employee", $Code);
-  	foreach ($result as $item) {
-  		$Code = $item['Code'];
-  		$Name = $item['Name'];
-  		$DataOfBirth = $item['DataOfBirth'];
 
-  	}
-	
+	require_once "DAO/DBConnect.php";
+
+	$name = $_POST['name'];
+
+	$result = array();
+  	$result = Edit("employee", "Code",$name);
+		
+	/*	$array = array(
+			'Code' => $fetch['Code'],
+			'Name' => $fetch['Name'],
+			'DateOfBirth' => $fetch['DateOfBirth'],
+			'Gender' => $fetch['Gender'],
+			'Position' => $fetch['Position'],
+			'PhoneNumber' => $fetch['PhoneNumber'],
+			'Email' => $fetch['Email'],
+			'Address' => $fetch['Address'],
+		);
+		
+		echo json_encode($array);
+		*/
+	echo json_encode($result);
 ?>
