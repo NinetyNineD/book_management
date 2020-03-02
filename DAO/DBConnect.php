80c5a704004879b51b5f27e4d9287704b787d29d
NinetyNineD <nguyentandung.24h99p@gmail.com>
2020-03-02 23:06:30 +0700

    Create function runQuery and numRows, class PerPage

--- DAO/DBConnect.php
+++ DAO/DBConnect.php
@@ -97,3 +97,81 @@ function searchEmployee($Table,$Search,$ID){
     }
     return $result;
 }
+    function runQuery($query) {
+        global $conn;
+        Connect();
+        $result = mysqli_query($conn,$query);
+        while($row=mysqli_fetch_assoc($result)) {
+            $resultset[] = $row;
+        }       
+        if(!empty($resultset))
+            return $resultset;
+    }
+    
+    function numRows($query) {
+        global $conn;
+        Connect();
+        $result  = mysqli_query($conn,$query);
+        $rowcount = mysqli_num_rows($result);
+        return $rowcount;   
+    }
+//
+class PerPage {
+    public $perpage;
+    
+    function __construct() {
+        $this->perpage = 5;
+    }
+    
+    function getAllPageLinks($count,$href) {
+        $output = '';
+        if(!isset($_GET["page"])) $_GET["page"] = 1;
+        if($this->perpage != 0)
+            $pages  = ceil($count/$this->perpage);
+        if($pages>1) {
+            if($_GET["page"] == 1) 
+                $output = $output . '<span class="link first disabled">&#8810;</span><span class="link disabled">&#60;</span>';
+            else    
+                $output = $output . '<a class="link first" onclick="getresult(\'' . $href . (1) . '\')" >&#8810;</a><a class="link" onclick="getresult(\'' . $href . ($_GET["page"]-1) . '\')" >&#60;</a>';
+            
+            
+            if(($_GET["page"]-3)>0) {
+                if($_GET["page"] == 1)
+                    $output = $output . '<span id=1 class="link current">1</span>';
+                else                
+                    $output = $output . '<a class="link" onclick="getresult(\'' . $href . '1\')" >1</a>';
+            }
+            if(($_GET["page"]-3)>1) {
+                    $output = $output . '<span class="dot">...</span>';
+            }
+            
+            for($i=($_GET["page"]-2); $i<=($_GET["page"]+2); $i++)  {
+                if($i<1) continue;
+                if($i>$pages) break;
+                if($_GET["page"] == $i)
+                    $output = $output . '<span id='.$i.' class="link current">'.$i.'</span>';
+                else                
+                    $output = $output . '<a class="link" onclick="getresult(\'' . $href . $i . '\')" >'.$i.'</a>';
+            }
+            
+            if(($pages-($_GET["page"]+2))>1) {
+                $output = $output . '<span class="dot">...</span>';
+            }
+            if(($pages-($_GET["page"]+2))>0) {
+                if($_GET["page"] == $pages)
+                    $output = $output . '<span id=' . ($pages) .' class="link current">' . ($pages) .'</span>';
+                else                
+                    $output = $output . '<a class="link" onclick="getresult(\'' . $href .  ($pages) .'\')" >' . ($pages) .'</a>';
+            }
+            
+            if($_GET["page"] < $pages)
+                $output = $output . '<a  class="link" onclick="getresult(\'' . $href . ($_GET["page"]+1) . '\')" >></a><a  class="link" onclick="getresult(\'' . $href . ($pages) . '\')" >&#8811;</a>';
+            else                
+                $output = $output . '<span class="link disabled">></span><span class="link disabled">&#8811;</span>';
+            
+            
+        }
+        return $output;
+    }
+}
+?>
