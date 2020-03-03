f201659e46041b0daf37898c7e0f351ac2fb051e
Lưu Việt Anh <51351416+lva8hb4@users.noreply.github.com>
2020-03-03 21:36:38 +0700

    Sửa trang connect thay đổi tên csdl

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
-$connect = mysqli_connect("localhost","root","","qltv") or die("No Connect!");
+$connect = mysqli_connect("localhost","root","","bookmanagement") or die("No Connect!");
 $query_connect = mysqli_query($connect,"set names 'utf8'");
 ?>
